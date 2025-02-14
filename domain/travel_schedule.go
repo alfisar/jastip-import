748e15a5c1d76f8b3b07344c8fdb19b38c272fea
alfisar <alfisar.konsultan@work.bri.co.id>
2025-02-14 15:16:01 +0700

    [FIXING] : argumen gorm on struct travelschrequest

--- domain/travel_schedule.go
+++ domain/travel_schedule.go
@@ -1,17 +1,17 @@
 package domain
 
 type TravelSchRequest struct {
-	ID          int    `json:"id"`
-	UserID      int    `json:"user_id"`
-	Location    string `json:"location"`
-	PeriodStart string `json:"period_start"`
-	PeriodEnd   string `json:"period_end"`
-	Status      int    `json:"status"`
+	ID          int    `gorm:"column:id" json:"id"`
+	UserID      int    `gorm:"column:user_id" json:"user_id"`
+	Location    string `gorm:"column:locations" json:"location"`
+	PeriodStart string `gorm:"column:period_start" json:"period_start"`
+	PeriodEnd   string `gorm:"column:period_end" json:"period_end"`
+	Status      int    `gorm:"column:status" json:"status"`
 }
 
 type TravelSchResponse struct {
-	ID          int    `json:"id"`
-	Location    string `json:"location"`
-	PeriodStart string `json:"period_start"`
-	PeriodEnd   string `json:"period_end"`
+	ID          int    `gorm:"column:id" json:"id"`
+	Location    string `gorm:"column:locations" json:"location"`
+	PeriodStart string `gorm:"column:period_start" json:"period_start"`
+	PeriodEnd   string `gorm:"column:period_end" json:"period_end"`
 }
