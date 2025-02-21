bf534a26aac1ecf3fd663493da1ce7cd36b9a137
alfisar <alfisar.konsultan@work.bri.co.id>
2025-02-21 10:39:39 +0700

    [ENCHANCEMENT] : Fix CalculateOffsetAndLimit return

--- helpers/handler/repository.go
+++ helpers/handler/repository.go
@@ -3,21 +3,21 @@ package handler
 // CalculateOffsetAndLimit calculates the offset and limit based on the page number and page size
 func CalculateOffsetAndLimit(page, pageSize int) (pages int, offset int, limit int) {
 	if page < 1 {
-		pages = 1
+		page = 1
 	}
 
 	if pageSize < 1 {
 		pageSize = 10
 	}
 
-	offset = (pages - 1) * pageSize
+	offset = (page - 1) * pageSize
 	if pageSize > 1000 {
 		limit = 1000
 	} else {
 		limit = pageSize
 	}
-
-	return pages, offset, limit
+	pages = page
+	return page, offset, limit
 }
 
 // CalculateTotalPages calculates the total pages based on the number of items and page size
