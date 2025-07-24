6af517f75130045767b7ffe99eb1d6fc2cd552c8
alfisar <alfisar.konsultan@work.bri.co.id>
2025-07-24 10:08:51 +0700

    [CHANGE] : type data locations

--- domain/travel_schedule.go
+++ domain/travel_schedule.go
@@ -3,7 +3,7 @@ package domain
 type TravelSchRequest struct {
 	ID          int    `gorm:"column:id" json:"id"`
 	UserID      int    `gorm:"column:user_id" json:"user_id"`
-	Location    string `gorm:"column:locations" json:"location"`
+	Location    int    `gorm:"column:locations" json:"location"`
 	PeriodStart string `gorm:"column:period_start" json:"period_start"`
 	PeriodEnd   string `gorm:"column:period_end" json:"period_end"`
 	Status      int    `gorm:"column:status" json:"status"`
