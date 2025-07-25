995996ad0eaec28ef072739631d808cca3d64062
alfisar <alfisar.konsultan@work.bri.co.id>
2025-07-25 14:29:16 +0700

    [FIXING] : domain products

--- domain/product.go
+++ domain/product.go
@@ -5,22 +5,17 @@ type ProductData struct {
 	Name     string  `gorm:"name" json:"name"`
 	Desc     string  `gorm:"desc" json:"desc"`
 	Price    float64 `gorm:"price" json:"price"`
-	TravelID int     `gorm:"travel_schedule_id" json:"travel_schedule_id"`
 	Image    string  `gorm:"image" json:"image"`
 	Status   int     `gorm:"status" json:"status"`
 	Quantity int     `gorm:"quantity" json:"quantity"`
 }
 
 type ProductResp struct {
-	ID                int     `gorm:"id" json:"id"`
-	Name              string  `gorm:"name" json:"name"`
-	Desc              string  `gorm:"desc" json:"desc"`
-	Price             float64 `gorm:"price" json:"price"`
-	TravelID          int     `gorm:"travel_schedule_id" json:"travel_schedule_id"`
-	TravelLocation    int     `gorm:"location" json:"location"`
-	TravelPeriodStart string  `gorm:"column:period_start" json:"period_start"`
-	TravelPeriodEnd   string  `gorm:"column:period_end" json:"period_end"`
-	Image             string  `gorm:"image" json:"image"`
-	Status            int     `gorm:"status" json:"status"`
-	Quantity          int     `gorm:"quantity" json:"quantity"`
+	ID       int     `gorm:"id" json:"id"`
+	Name     string  `gorm:"name" json:"name"`
+	Desc     string  `gorm:"desc" json:"desc"`
+	Price    float64 `gorm:"price" json:"price"`
+	Image    string  `gorm:"image" json:"image"`
+	Status   int     `gorm:"status" json:"status"`
+	Quantity int     `gorm:"quantity" json:"quantity"`
 }
