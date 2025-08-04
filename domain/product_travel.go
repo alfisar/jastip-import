ee3620e2a62a4430d1385f75844934746b0ac7d8
alfisar <alfisar589@gmail.com>
2025-08-04 08:37:02 +0700

    [ADD] : domain ProductsTravelRequest

--- domain/product_travel.go
+++ domain/product_travel.go
@@ -5,3 +5,9 @@ type ProductsTravel struct {
 	ProductID int `json:"column:product_id" gorm:"product_id"`
 	TravelID  int `json:"column:treveler_schedule_id" gorm:"treveler_schedule_id"`
 }
+
+type ProductsTravelRequest struct {
+	ID        int   `json:"column:id" gorm:"id"`
+	ProductID []int `json:"column:product_id" gorm:"products_id"`
+	TravelID  []int `json:"column:treveler_schedule_id" gorm:"treveler_schedules_id"`
+}
