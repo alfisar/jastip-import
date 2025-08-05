32582e80c060d0e40b33a020bdf7939fdb21eea9
alfisar <alfisar589@gmail.com>
2025-08-05 15:54:05 +0700

    [FIXING] : Key on Product Travel

--- domain/product_travel.go
+++ domain/product_travel.go
@@ -3,11 +3,11 @@ package domain
 type ProductsTravel struct {
 	ID        int `json:"id" gorm:"column:id"`
 	ProductID int `json:"product_id" gorm:"column:product_id"`
-	TravelID  int `json:"treveler_schedule_id" gorm:"column:treveler_schedule_id"`
+	TravelID  int `json:"traveler_schedule_id" gorm:"column:traveler_schedule_id"`
 }
 
 type ProductsTravelRequest struct {
 	ID        int   `json:"id" gorm:"column:id"`
 	ProductID []int `json:"product_id" gorm:"column:products_id"`
-	TravelID  []int `json:"treveler_schedule_id" gorm:"column:treveler_schedules_id"`
+	TravelID  []int `json:"traveler_schedule_id" gorm:"column:traveler_schedules_id"`
 }
